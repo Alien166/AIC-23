28d2a36ddb81420d8763bd8688c469f6cf4ee8ca
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:08:45 +0300

    Create B. Basic Data Types.cpp

---
+++ Newcomers-training/sheet-1/B. Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    int a;
+    cin>>a;
+    ll b;
+    cin>>b;
+    char c;
+    cin>>c;
+    float d;
+    cin>>d;
+    double f;
+    cin>>f;
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl<<f<<endl;
+
+}
