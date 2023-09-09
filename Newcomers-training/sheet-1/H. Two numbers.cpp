869dfbe92c98462a4e0d0af61968f573d48ea52b
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:11:43 +0300

    Create H. Two numbers.cpp

---
+++ Newcomers-training/sheet-1/H. Two numbers.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+  float n,m;
+  cin>>n>>m;
+  cout<<"floor "<<n<<" / "<<m<<" = "<<floor(n/m)<<endl;
+  cout<<"ceil "<<n<<" / "<<m<<" = "<<ceil(n/m)<<endl;
+  cout<<"round "<<n<<" / "<<m<<" = "<<round(n/m)<<endl;
+
+
+
+}
+ 
