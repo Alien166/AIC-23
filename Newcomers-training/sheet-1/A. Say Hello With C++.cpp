fff5d5bfa19db8dacb696dec32d90dbd25c35bcc
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:07:52 +0300

    Create A. Say Hello With C++.cpp

---
+++ Newcomers-training/sheet-1/A. Say Hello With C++.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+void solve(string s) {
+    cout<<"Hello, "<<s;
+}
+int main()
+{
+    string s;
+    cin>>s;
+    solve(s);
+
+}
