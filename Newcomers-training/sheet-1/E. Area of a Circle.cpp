2fb75fb288890cb3017963049857df290d2dc757
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:10:20 +0300

    Create E. Area of a Circle.cpp

---
+++ Newcomers-training/sheet-1/E. Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+   double r;
+   cin>>r;
+   double pi=3.141592653;
+   double result = r * r *pi;
+   cout<<fixed<<setprecision(9)<<result;
+
+
+}
