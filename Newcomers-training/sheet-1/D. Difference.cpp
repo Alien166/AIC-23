22685f3fedf230d7945df27a92dc45f3760e5f3c
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:09:51 +0300

    Create D. Difference.cpp

---
+++ Newcomers-training/sheet-1/D. Difference.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+   ll a,b,c,d,result;
+   cin>>a>>b>>c>>d;
+   result=(a*b)-(c*d);
+   cout<<"Difference = "<<result;
+
+
+}
