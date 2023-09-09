ce1a4a43950dde84689b6569556e6ad5a086d7e4
Abdeltawab Wagieh <98906055+Alien166@users.noreply.github.com>
2023-09-09 19:09:17 +0300

    Create C. Simple Calculator.cpp

---
+++ Newcomers-training/sheet-1/C. Simple Calculator.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+   ll a,b;  cin>>a>>b;
+   cout<<a<<" + "<<b<<" = "<<a+b<<"\n";
+   cout<<a<<" * "<<b<<" = "<<a*b<<"\n";
+   cout<<a<<" - "<<b<<" = "<<a-b<<"\n";
+
+}
